b9d9a46aa2217222ba31d3ebf9d02574e7aa3d46
Joao <joaoded@hotmail.com>
2019-06-07 17:54:49 -0300

    New features

--- App.js
+++ App.js
@@ -5,18 +5,9 @@ import Explore from './screens/Explore'
 import Inbox from './screens/Inbox'
 import Saved from './screens/Saved'
 import Trips from './screens/Trips'
+import Profile from './screens/Profile'
 import Icon from 'react-native-vector-icons/Ionicons'
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
 
 export default createBottomTabNavigator({
   Explore: {
@@ -60,10 +51,18 @@ export default createBottomTabNavigator({
     navigationOptions: {
       tabBarLabel: 'PROFILE',
       tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-chatboxes" color={tintColor} size={24} />
+        <Icon name="ios-person" color={tintColor} size={24} />
       )
     }
-  }
+  },
+  /* tabBarOptions: {
+    activeTintColor: 'red',
+    inactiveTintColor: 'grey',
+    style: {
+      backgroundColor: 'white',
+      borderTopWidth: 0
+    }
+  } */
 })
 
 const styles = StyleSheet.create({
--- screens/Explore.js
+++ screens/Explore.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native'
+
+import Icon from 'react-native-vector-icons/Ionicons'
+
 
 class Explore extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Explore</Text>
-      </View>
+      <SafeAreaView>
+        <View style={{flex: 1}}>
+          <View style={{height: 80, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#dddddd'}}>
+            <View style={{flexDirection: 'row', padding: 10, backgroundColor: 'white', marginHorizontal: 20,
+              shadowOffset: {width: 0, height: 0},
+              shadowColor: 'black',
+              shadowOpacity: 0.2,
+              elevation: 1
+          }}>
+              <Icon name="ios-search" size={20} style={{marginRight: 10}} />
+              <TextInput
+                underlineColorAndroid="transparent"
+                placeholder="Try new "
+                placeholderTextColor="grey"
+                style={{flex: 1, fontWeight: '700', backgroundColor: 'white'}}
+              />
+            </View>
+          </View>
+
+        </View>
+      </SafeAreaView>
     );
   }
 }
